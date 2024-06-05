2e228eeae8ed83748ff6f4f2b4f7d07e43a72d9e
Your Name <rak2042@daum.net>
2024-06-05 14:49:34 +0900

    App.java

---
+++ App.java
@@ -0,0 +1,25 @@
+class App{
+public static void main(String[] args){
+
+int kor1;
+int kor2;
+int kor3;
+int total;
+float avg;
+
+kor1 = 30;
+kor2 = 40;
+kor3 = 50;
+
+total = kor1 + kor2 + kor3;
+avg = total;
+
+System.out.printf("kor1 is %d\n", kor1);
+System.out.printf("kor2 is %d\n", kor2);
+System.out.printf("kor3 is %d\n", kor3);
+System.out.printf("total is %d\n", total);
+System.out.printf("avg is %.2f\n", avg);
+
+}
+}
+
